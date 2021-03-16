a3c9d13203de24f5d188c34d09048d0d0e566982
unknown <johnyun930@gmail.com>
2021-03-15 19:08:05 -0700

    change server url as sub domain because of the cookie problem

--- src/states/DomainContext.ts
+++ src/states/DomainContext.ts
@@ -1,4 +1,4 @@
 import {createContext} from 'react';
 
- export const DomainContext = createContext<string>("https://lifewaygen.herokuapp.com/");
+ export const DomainContext = createContext<string>("http://server.lifewaygen.ga/");
 //  export const DomainContext = createContext<string>("http://localhost:8000");
