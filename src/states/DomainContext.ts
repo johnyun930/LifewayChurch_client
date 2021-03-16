f8f0494b5aee6293de5d9367816f3060edf624c3
unknown <johnyun930@gmail.com>
2021-03-15 18:20:51 -0700

    change to web domain

--- src/states/DomainContext.ts
+++ src/states/DomainContext.ts
@@ -1,4 +1,4 @@
 import {createContext} from 'react';
 
-// export const DomainContext = createContext<string>("https://immense-beach-32425.herokuapp.com");
- export const DomainContext = createContext<string>("http://localhost:8000");
+ export const DomainContext = createContext<string>("https://lifewaygen.herokuapp.com/");
+//  export const DomainContext = createContext<string>("http://localhost:8000");
