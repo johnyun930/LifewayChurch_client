e79b33b3196d24431c38173d51d7b03277e5b475
unknown <johnyun930@gmail.com>
2021-03-15 23:47:04 -0700

    change as https

--- src/states/DomainContext.ts
+++ src/states/DomainContext.ts
@@ -1,4 +1,4 @@
 import {createContext} from 'react';
 
- export const DomainContext = createContext<string>("http://server.lifewaygen.ga/");
+ export const DomainContext = createContext<string>("https://server.lifewaygen.ga/");
 //  export const DomainContext = createContext<string>("http://localhost:8000");
