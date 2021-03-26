a4acb4d64a73f16ed17c2587350fa9ceebaa8d27
unknown <johnyun930@gmail.com>
2021-03-26 01:40:21 -0700

    Add image and change the context

--- src/states/DomainContext.ts
+++ src/states/DomainContext.ts
@@ -1,4 +1,4 @@
 import {createContext} from 'react';
 
-  // export const DomainContext = createContext<string>(process.env.REACT_APP_DOMAIN!);
- export const DomainContext = createContext<string>("http://localhost:8000");
+  export const DomainContext = createContext<string>(process.env.REACT_APP_DOMAIN!);
+//  export const DomainContext = createContext<string>("http://localhost:8000");
